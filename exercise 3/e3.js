9bc9f043152a4ba37ccabecf9da7b0ba8635d0cf
salmaabdirisak029-tech <salmaabdirisak029@gmail.com>
2025-12-25 21:37:36 +0300

    Exercise 3  JavaScript

---
+++ exercise 3/e3.js
@@ -0,0 +1,15 @@
+let IsSunny = true;
+let isWeakend = false;
+
+console.log(IsSunny && isWeakend);
+console.log(IsSunny || isWeakend);
+console.log(!IsSunny);
+console.log(!isWeakend);
+
+
+
+
+
+
+
+
