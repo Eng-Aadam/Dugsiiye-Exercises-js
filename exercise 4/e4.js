4c7977787374f51958c2cd77306675c73c318316
salmaabdirisak029-tech <salmaabdirisak029@gmail.com>
2026-01-02 17:17:32 +0300

    Exercise 4 JavaScript

---
+++ exercise 4/e4.js
@@ -0,0 +1,24 @@
+// Function Declaration
+function add(a, b){
+    console.log(a + b);
+}
+
+add(10, 20);
+add(5, 15);
+add(7, 8);
+
+
+
+// Function expression
+
+const greet = function(a, b){
+    console.log("iskudarka " + (a + b));
+};
+greet(25, 40);
+greet(55, 10);
+greet(30, 25);
+
+
+
+
+
