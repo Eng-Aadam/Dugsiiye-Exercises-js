a149a28a2d8f7b8e9d6374656fec09735fe3d96c
salmaabdirisak029-tech <salmaabdirisak029@gmail.com>
2026-01-06 16:05:44 +0300

    Exercise 7 JavaScript

---
+++ exercise 7/e7.js
@@ -0,0 +1,26 @@
+
+// Arrays Methods
+
+let colors = ["red", "blue", "green", "yellow"];
+//before push
+colors.push("purple");
+console.log(colors);
+
+// pop after push
+
+colors.pop(); 
+console.log(colors);
+
+
+
+//unshift before pop
+colors.unshift("orange");
+console.log(colors);
+
+//shift after unshift
+colors.shift(); 
+console.log(colors);
+
+
+//length
+console.log(colors.length);
